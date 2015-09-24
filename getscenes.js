168d0bd1b335cc195df35e208939c4cc7646faf6
Anand Rajagopal <anand_rajagopal@ischool.berkeley.edu>
2015-09-24 16:02:53 -0700

    adding get scenes with params

--- getscenes.js
+++ getscenes.js
@@ -1,6 +1,8 @@
+function getScenes(latitude,longitude){
+
 var scene_ids = [];
 var time_stamps = [];
-var url2 = "https://api.urthecast.com/v1/archive/scenes?api_key=25DD9D131871413DB0CB&api_secret=24042ED40BA7403CA7831CBE46B84EC9&geometry_intersects=POINT(-122.27508%2037.81195)";
+var url2 = "https://api.urthecast.com/v1/archive/scenes?api_key=25DD9D131871413DB0CB&api_secret=24042ED40BA7403CA7831CBE46B84EC9&geometry_intersects=POINT("+latitude+"%20"+longitude+")";
 var xmlhttp2 = new XMLHttpRequest();
 
         xmlhttp2.open("GET", url2, true);
@@ -12,17 +14,11 @@ var xmlhttp2 = new XMLHttpRequest();
 
                var resp = JSON.parse(xmlhttp2.responseText);
            	   myArr2 = resp.payload
-               console.log(myArr2);
                
-               //console.log(myArr2[1].owner_scene_id);
-               //console.log(myArr2.length);
-
                for(i = 0; i < myArr2.length; i++) {
                	scene_ids[i] = (myArr2[i].acquired.substring(0,10));
                	time_stamps[i] = (myArr2[i].owner_scene_id);
                }
                var scene_properties = [time_stamps,scene_ids];
-               console.log(scene_properties);
-               console.log(scene_ids);
-               console.log(time_stamps);
-               }}
+      }};
+}
