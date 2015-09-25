9d99534bf360952b9864b9407c422504368f2ab8
ajones620 <jalexander620@gmail.com>
2015-09-24 18:26:56 -0700

    hackathon demo version

--- demo1.js
+++ demo1.js
@@ -8,7 +8,7 @@ apiSecret = "847426A8A04C425BB377CE8BAC26F48D"
 var map = L.map('map').setView([
     37.78684346730307,
     -122.40559101104735
-], 9);
+], 12);
 
 // Create a simple UC tile layer - global map, no restrictions
 // var url = "https://tile-{s}.urthecast.com/v1/rgb/{z}/{x}/{y}?api_key="+apiKey+"&api_secret="+apiSecret;
@@ -18,6 +18,8 @@ var map = L.map('map').setView([
 
 var filterType = 'rgb';
 var sceneId = 'Fo4XzqtWQkaLO2jj-igVbA';
+
+var locationName = '';
 
 var baseLayer = new L.tileLayer("https://tile-{s}.urthecast.com/v1/rgb/{z}/{x}/{y}?api_key="+apiKey+"&api_secret="+apiSecret);
 var sceneLayer = new L.tileLayer("https://tile-{s}.urthecast.com/v1/"+filterType+"/{z}/{x}/{y}?api_key="+apiKey+"&api_secret="+apiSecret+'&id='+sceneId);
@@ -60,22 +62,31 @@ function updateScene(newSceneId) {
 var playScenesCounter = 0;
 var updatedSceneData = '';
 
+var playScenesFlag = false;
+
+
 function playScenes(sceneData) {
 
-    setTimeout(function () {    
-        
-    updateScene(sceneData[1][playScenesCounter]);
+    setTimeout(function () {  
 
-    playScenesCounter++;                    
-      if (playScenesCounter < sceneData[0].length) {           
-         playScenes(sceneData);         
-      }
+    if (playScenesFlag === true) {  
+            
+        updateScene(sceneData[1][playScenesCounter]);
 
-      else if (playScenesCounter == sceneData[0].length) {
-        playScenesCounter = 0;
-        playScenes(sceneData);
-      }                       
-   }, 3000);
+        playScenesCounter++;
+
+          if (playScenesCounter < sceneData[0].length) {
+
+            $('#overlay').html(locationName + " on " + sceneData[0][playScenesCounter]);           
+             playScenes(sceneData);         
+          }
+
+          else if (playScenesCounter == sceneData[0].length) {
+            playScenesCounter = 0;
+            playScenes(sceneData);
+          }                       
+       }
+    }, 3000);
 }
 
 function updateSceneData(sceneData) {
@@ -100,6 +111,8 @@ function addBounce(map){
 
         center = [lat, lng];
 
+        console.log(center);
+
         map.setView(center, map.getZoom());
 
         marker = new L.Marker([lat, lng], {bounceOnAdd: true}).addTo(map);
@@ -107,6 +120,7 @@ function addBounce(map){
 
         var sceneData = getScenes(String(lat), String(lng), updateSceneData);
 
+        getLocationName(lat, lng);
         
         marker.on('click', function () {
             marker.bounce({duration: 500, height: 100});
@@ -117,15 +131,69 @@ function addBounce(map){
 
 $('.filterButton').on('click', function() {
 
+    $('.filterButton').css({'background': '#ffffff', 'color': '#222222'});
+
+    $(this).css({'background': '#222222', 'color': '#ffffff'});
+
     playScenesCounter = 0;
 
 	switchLayer(this);
+
 });
+
+function getLocationName(lat, lng) {
+    var url2 = "http://photon.komoot.de/reverse?lon="+lng+"8&lat="+lat;
+    var xmlhttp2 = new XMLHttpRequest();
+
+       xmlhttp2.open("GET", url2, true);
+       xmlhttp2.send();
+
+       xmlhttp2.onreadystatechange = function() {
+       if (xmlhttp2.readyState == 4 &&
+              xmlhttp2.status == 200) {
+
+              var resp = JSON.parse(xmlhttp2.responseText);
+                 //myArr2 = resp.payload
+              //console.log(myArr2);
+
+              console.log(resp);
+              console.log(resp.features[0].properties.city);
+
+              locationName = resp.features[0].properties.city;
+              $('#overlay').html(locationName);
+              //console.log(myArr2[1].owner_scene_id);
+              //console.log(myArr2.length);
+
+              /*for(i = 0; i < myArr2.length; i++) {
+                  scene_ids[i] = (myArr2[i].acquired.substring(0,10));
+                  time_stamps[i] = (myArr2[i].owner_scene_id);
+              }
+              var scene_properties = [time_stamps,scene_ids];
+              console.log(scene_properties);
+              console.log(scene_ids);
+              console.log(time_stamps);*/
+              }}
+}
 
 
 $('#playScenes').on('click', function() {
 
-    playScenes(updatedSceneData);
+    if (playScenesFlag === false) {
+
+        playScenesFlag = true;
+
+        playScenes(updatedSceneData);
+
+        $('#playScenes').html('pause');
+
+    }
+
+    else if (playScenesFlag === true) {
+
+        playScenesFlag = false;
+
+        $('#playScenes').html('play');
+    }
 });
 
 
--- getscenes.js
+++ getscenes.js
@@ -6,7 +6,7 @@ var scene_properties;
 
 var scene_ids = [];
 var time_stamps = [];
-var url2 = "https://api.urthecast.com/v1/archive/scenes?api_key=25DD9D131871413DB0CB&api_secret=24042ED40BA7403CA7831CBE46B84EC9&geometry_intersects=POINT("+longitude+"%20"+latitude+")";
+var url2 = "https://api.urthecast.com/v1/archive/scenes?api_key=25DD9D131871413DB0CB&api_secret=24042ED40BA7403CA7831CBE46B84EC9&cloud_coverage_lte=20&geometry_intersects=POINT("+longitude+"%20"+latitude+")";
 var xmlhttp2 = new XMLHttpRequest();
 
         xmlhttp2.open("GET", url2, true);
